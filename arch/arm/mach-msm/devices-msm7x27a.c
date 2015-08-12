68384daf32dcd0855ae774cd91ed03be1d117b15
RolanDroid <rolandosimoncelli8@gmail.com>
2015-08-12 12:57:22 +0200

    Revert "Fixed gpu overclock"
    This reverts commit 814290db6ef90459181e0fedbd712464ba455457.

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -913,12 +913,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.gpu_freq = 400000000,
 			.bus_freq = 220000000,
 		},
-		#ifdef CONFIG_KGSL_GPU_OVERCLOCK
+		#elif CONFIG_KGSL_GPU_OVERCLOCK
 		{
 			.gpu_freq = 350000000,
 			.bus_freq = 210000000,
 		},
-		#endif
+		#else
 		/* LGE_CHANGE_S [peter.jung@lge.com]
 		    DoU Power consumption */
 		{
