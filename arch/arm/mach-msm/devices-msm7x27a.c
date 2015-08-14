099ee8358356469905fcf9384d4ac1be036c5130
RolanDroid <rolandosimoncelli8@gmail.com>
2015-08-14 20:47:05 +0200

    GPU:Overclock to 400Mhz stable
    Deleted 450Mhz
    
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -911,13 +911,13 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		/* LGE_CHANGE_S [peter.jung@lge.com]
 		    DoU Power consumption */
 		{
-			.gpu_freq = 450000000,
-                        .bus_freq = 225000000,
+			.gpu_freq = 400000000,
+                        .bus_freq = 200000000,
 		},
 		/* LGE_CHANGE_E [peter.jung@lge.com] */
 		{
-			.gpu_freq = 400000000,
-			.bus_freq = 200000000,
+			.gpu_freq = 350000000,
+			.bus_freq = 175000000,
 		},
 		{
 			.gpu_freq = 300000000,
