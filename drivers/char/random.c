5577f7b89cb5ee890c3790c557da768299c84d7e
RolanDroid <rolandosimoncelli8@gmail.com>
2015-08-18 14:36:50 +0200

    Revert "random: prevent add_input from doing anything"
    This reverts commit bc1f42844f628c9db3dbbbba61cfabee60897312.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -655,6 +655,8 @@ void add_device_randomness(const void *buf, unsigned int size)
 }
 EXPORT_SYMBOL(add_device_randomness);
 
+static struct timer_rand_state input_timer_state;
+
 /*
  * This function adds entropy to the entropy "pool" by using timing
  * delays.  It uses the timer_rand_state structure to make an estimate
@@ -727,7 +729,16 @@ out:
 void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value)
 {
-	return;
+	static unsigned char last_value;
+
+	/* ignore autorepeat and the like */
+	if (value == last_value)
+		return;
+
+	DEBUG_ENT("input event\n");
+	last_value = value;
+	add_timer_randomness(&input_timer_state,
+			     (type << 4) ^ code ^ (code >> 4) ^ value);
 }
 EXPORT_SYMBOL_GPL(add_input_randomness);
 
